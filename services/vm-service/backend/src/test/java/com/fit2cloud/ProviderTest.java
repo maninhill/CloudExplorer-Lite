c82c23f07e7f77c5d294cc14f0837539abf52650
jianneng-fit2cloud <jianneng.pan@fit2cloud.com>
2022-10-16 20:07:55 +0800

    feat: 支持阿里云开关机重启删除操作,虚拟机操作添加任务

--- services/vm-service/backend/src/test/java/com/fit2cloud/ProviderTest.java
+++ services/vm-service/backend/src/test/java/com/fit2cloud/ProviderTest.java
@@ -2,7 +2,13 @@ package com.fit2cloud;
 
 import com.fit2cloud.base.service.IBaseCloudAccountService;
 import com.fit2cloud.common.scheduler.SchedulerService;
+import com.fit2cloud.common.utils.JsonUtil;
+import com.fit2cloud.provider.impl.aliyun.AliyunCloudProvider;
+import com.fit2cloud.provider.impl.aliyun.entity.credential.AliyunVmCredential;
+import com.fit2cloud.provider.impl.aliyun.entity.request.AliyunInstanceRequest;
+import com.fit2cloud.provider.impl.aliyun.entity.request.ListVirtualMachineRequest;
 import com.fit2cloud.service.ISyncProviderService;
+import com.fit2cloud.service.IVmCloudServerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -28,6 +34,9 @@ public class ProviderTest {
     private SchedulerService schedulerService;
     @Resource
     private IBaseCloudAccountService cloudAccountService;
+
+    @Resource
+    private IVmCloudServerService serverService;
     @Test
     public void init(){
         cloudAccountService.initCloudAccountJob("9c320c339c56b62707b4fe440163fbd1");
@@ -60,5 +69,34 @@ public class ProviderTest {
         syncProviderService.syncCloudServer("e6867e8dfb2b19747117cdc698f58cbc");
 //        syncProviderService.syncCloudImage("e6867e8dfb2b19747117cdc698f58cbc");
 //        syncProviderService.syncCloudDisk("e6867e8dfb2b19747117cdc698f58cbc");
+    }
+
+    @Test
+    public void operate(){
+        AliyunVmCredential credential = new AliyunVmCredential();
+        credential.setAccessKeyId("LTAI4GDCfBgxQQaD6av4zwxP");
+        credential.setAccessKeySecret("glUNNlZhTKylD5DbexuPAJwzTSwYNv");
+        AliyunCloudProvider cp = new AliyunCloudProvider();
+        AliyunInstanceRequest req = new AliyunInstanceRequest();
+        req.setCredential(JsonUtil.toJSONString(credential));
+        req.setRegionId("cn-guangzhou");
+        req.setUuId("i-7xv5tev4y2j9fpc84dgz");
+        //System.out.println("关机："+cp.powerOff(JsonUtil.toJSONString(req)));
+        //System.out.println("开机："+cp.powerOn(JsonUtil.toJSONString(req)));
+        //System.out.println("重启："+cp.rebootInstance(JsonUtil.toJSONString(req)));
+        System.out.println("删除："+cp.deleteInstance(JsonUtil.toJSONString(req)));
+
+    }
+
+    @Test
+    public void searchVM(){
+        AliyunVmCredential credential = new AliyunVmCredential();
+        credential.setAccessKeyId("LTAI4GDCfBgxQQaD6av4zwxP");
+        credential.setAccessKeySecret("glUNNlZhTKylD5DbexuPAJwzTSwYNv");
+        AliyunCloudProvider cp = new AliyunCloudProvider();
+        ListVirtualMachineRequest request = new ListVirtualMachineRequest();
+        request.setCredential(JsonUtil.toJSONString(credential));
+        request.setRegionId("cn-beijing");
+        System.out.println(JsonUtil.toJSONString(cp.listVirtualMachine(JsonUtil.toJSONString(request))));
     }
 }
